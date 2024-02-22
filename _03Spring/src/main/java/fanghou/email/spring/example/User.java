b941728d08258abf456863b8efaba6e75cbda257
FangHou <hf5902383@gmail.com>
2024-02-22 17:25:29 +0800

    Day3 commit xml练习

--- _03Spring/src/main/java/fanghou/email/spring/example/User.java
+++ _03Spring/src/main/java/fanghou/email/spring/example/User.java
@@ -1,5 +1,10 @@
 package fanghou.email.spring.example;
+import org.springframework.stereotype.Component;
 
+//吧User class 通过注解的方式 交给框架
+@Component
 public class User {
-
+    public void run(){
+        System.out.println("Hello, Spring Framework");
+    }
 }
